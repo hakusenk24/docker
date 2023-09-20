8431b617a7f1388cc955a6ec6bba20da4a30315d
hakusenk24 <hakusen.k24@gmail.com>
2023-09-20 09:42:27 +0700

    add dockerignore template

--- script/docker_run.sh
+++ script/docker_run.sh
@@ -2,21 +2,21 @@
 if [[ "$ENV_VAR" = "dev" || "$ENV_VAR" = "local" ]]
 then
      docker run -d \
-        --name maytrics_chat_agentbot \
+        --name app_name \
         -p 8080:8080 \
         --env-file .env.local	\
         ### same network with host
         --net=host \
         ###
-        maytrics_chat_agentbot_image
+        image_name
 else
     if [[ "$ENV_VAR" = "prod" ]]
     then
         docker run -d \
-            --name maytrics_chat_agentbot \
+            --name app_name \
             -p 8080:8080 \
             --env-file .env	\
-            maytrics_chat_agentbot_image
+            image_name
     else
         echo Missing ENV_VAR
     fi
